5e71211f88d49826d1dcb6e5ac38af38711a7d83
Mark Page <mark.page@whitelamp.com>
2018-12-03 00:15:53 +0000

    A blank class

---
+++ hpapi-dba.js
@@ -0,0 +1,18 @@
+/* Copyright 2018 Whitelamp http://www.whitelamp.com/ */
+
+export class HpapiDba {
+
+    constructor () {
+    }
+
+    init (api) {
+        console.log ('Greetings human; this is HpapiDba speaking');
+        this.api        = api;
+        this.vendor     = 'whitelamp-uk';
+        this.package    = 'hpapi-dba-server';
+        console.log ('Client configuration:');
+        console.table (this.api.cfg);
+    }
+
+}
+
