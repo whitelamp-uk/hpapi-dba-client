4c4840bef42421a5fa1e595c0ccfc61117fd5376
Mark Page <mark.page@whitelamp.com>
2018-12-06 23:01:28 +0000

    First contact with API

--- hpapi-dba.js
+++ hpapi-dba.js
@@ -2,11 +2,28 @@
 
 export class HpapiDba {
 
-    constructor (api) {
+    constructor (api,templates) {
         this.api        = api;
         this.vendor     = 'whitelamp-uk';
         this.package    = 'hpapi-dba-server';
+        this.templates  = templates;
         this.api.log ('Greetings human; this is HpapiDba speaking');
+    }
+
+    rowsSelect (email,spec) {
+    var request = {
+            "email" : email,
+            "method": {
+                "vendor": "whitelamp-uk",
+                "package": "hpapi-dba-server",
+                "class": "\\Hpapi\\Dba",
+                "method": "rowsSelect",
+                "arguments": [
+                    spec
+                ]
+            }
+        }
+        return this.api.request (request);
     }
 
 }
